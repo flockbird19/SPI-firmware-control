6bff5e169f6f22555de570be3b1a32b6e2d7b3fe
sanjanabhatul-lab <sanjana.bhat.ul@gmail.com>
2026-08-21 11:28:45 +0530

    Fixed spi_master

--- src/spi_master.v
+++ src/spi_master.v
@@ -4,31 +4,41 @@ module spi_master(
     input wire enable,
     input wire load,
     input wire [7:0] parl_in,
+
     output wire mosi,
     output wire sclk,
+    output wire cs,
     output wire byte_done
 );
 
-    clk_div #(.divide(2)) u_clk_div(
-        .clk(clk),
-        .reset(reset),
-        .enable(enable),
-        .sclk(sclk)
+    // SPI Clock Divider
+    clk_div #(.divide(2)) u_clk_div (
+        .clk    (clk),
+        .reset  (reset),
+        .enable (enable),
+        .sclk   (sclk)
     );
 
-    piso_reg u_piso(
-        .sclk(sclk),
-        .reset(reset),
-        .load(load),
-        .parl_in(parl_in),
-        .serl_out(mosi)
+    // PISO Register
+    piso_reg u_piso (
+        .sclk     (sclk),
+        .reset    (reset),
+        .load     (load),
+        .parl_in  (parl_in),
+        .serl_out (mosi)
     );
 
-    bit_counter u_bit_counter(
-        .clk(clk),
-        .reset(reset),
-        .enable(enable),
-        .count(),
-        .done(byte_done)
+    // Bit Counter
+    bit_counter u_bit_counter (
+        .clk    (sclk),
+        .reset  (reset),
+        .enable (enable),
+        .count  (),
+        .done   (byte_done)
     );
+
+    // Chip Select
+    // SPI flash is active LOW
+    assign cs = ~enable;
+
 endmodule
